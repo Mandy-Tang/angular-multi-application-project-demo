cc5bd2049a4addad82fa7f53ef95fae0fa739a1c
Tang Man <189658646@qq.com>
2020-03-17 20:55:46 +0800

    use hello world service in both application

--- projects/dependences/src/lib/hello-world.service.ts
+++ projects/dependences/src/lib/hello-world.service.ts
@@ -6,4 +6,8 @@ import { Injectable } from '@angular/core';
 export class HelloWorldService {
 
   constructor() { }
+
+  public sayHelloWorldTo = (name: string) => {
+    return `Hello World ${name}`;
+  }
 }
--- projects/second/src/app/app.component.ts
+++ projects/second/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HelloWorldService } from 'projects/dependences/src/lib/hello-world.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'second';
+
+  constructor(private helloWorldService: HelloWorldService) {
+    this.title = `${this.helloWorldService.sayHelloWorldTo('Mandy')}, ${this.title} `;
+  }
 }
