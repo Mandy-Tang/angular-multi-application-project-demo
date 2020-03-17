a950708d232f81a196d361e06f095dc7c99eb654
Tang Man <189658646@qq.com>
2020-03-17 21:22:59 +0800

    create two page in child application and add the route

--- projects/child/src/app/app-routing.module.ts
+++ projects/child/src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { PageTwoComponent } from './page-two/page-two.component';
+import { PageOneComponent } from "./page-one/page-one.component";
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "page-one",
+    component: PageOneComponent
+  },
+  {
+    path: "page-two",
+    component: PageTwoComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
