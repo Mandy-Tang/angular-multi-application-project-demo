69440e87b72d58834aa09f8ed6a3e6562400b900
Tang Man <189658646@qq.com>
2020-03-18 11:20:09 +0800

    lazy load child in first app

--- projects/first/src/app/app-routing.module.ts
+++ projects/first/src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { PageThirdComponent } from './page-third/page-third.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
 const routes: Routes = [
   {
-    path: "page-third",
+    path: 'page-third',
     component: PageThirdComponent
+  },
+  {
+    path: 'child',
+    loadChildren: () =>
+      import('./../../../child/src/app/app.module').then(m => m.AppModule)
   }
 ];
 
@@ -14,4 +18,4 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
